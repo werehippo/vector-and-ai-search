29b0c654c1797acefddd5fd3ab0dcbe2ebce3a61
Linh Truong <linh.van.truong@outlook.com>
2025-07-31 22:01:19 +0700

    Add some helpful middlewares to Hono app

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,15 @@
 import 'dotenv/config';
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { logger as loggerMiddleware } from 'hono/logger';
+import { timeout } from 'hono/timeout';
 import { getCompletion } from '../lib/gemini.js';
 import { env } from '../services/env.js';
 import { logger } from '../services/logger.js';
 
-const app = new Hono();
-
-app.get('/health', (c) => {
-  return c.json({
-    status: 'ok',
-    timeStamp: new Date().toISOString(),
-  });
-});
-
-app.get('/api/v1/chat-completion', async (c) => {
+const apiV1 = new Hono();
+apiV1.get('/chat-completion', async (c) => {
   const { prompt } = c.req.query();
 
   const sanitizedPrompt = (prompt || '').trim();
@@ -30,6 +25,32 @@ app.get('/api/v1/chat-completion', async (c) => {
   const completion = await getCompletion(sanitizedPrompt);
   return c.json({ completion });
 });
+
+const app = new Hono()
+  .use(
+    loggerMiddleware((message: string, ...rest: string[]) => {
+      logger.info(message, ...rest);
+    })
+  )
+  .use(cors())
+  .use(timeout(10_000))
+  .notFound((c) => {
+    return c.json(
+      {
+        error: 'Not Found',
+      },
+      404
+    );
+  });
+
+app
+  .get('/health', (c) => {
+    return c.json({
+      status: 'ok',
+      timeStamp: new Date().toISOString(),
+    });
+  })
+  .route('/api/v1', apiV1);
 
 serve(
   {
